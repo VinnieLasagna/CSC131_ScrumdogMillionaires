0b13f223b28c8254ae85126c799d0ea315c1819b
Nicole Monroy <mmonroy2@student.sierracollege.edu>
2020-10-27 16:58:36 -0700

    class for json parsing

---
+++ src/MovieSearch.java
@@ -0,0 +1,12 @@
+import org.json.simple.JSONObject;
+
+//import java.io.BufferedReader;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+
+
+public MovieSearch
+        {
+
+        }
