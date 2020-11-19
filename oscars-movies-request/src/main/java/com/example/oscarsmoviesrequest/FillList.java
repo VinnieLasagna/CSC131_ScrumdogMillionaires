34843f90c246ec6a4d4edc879ee632025365cf48
Carter Bryant <donny.lao64@gmail.com>
2020-11-19 15:59:11 -0800

    updating program to send post requests and retrieve info

--- oscars-movies-request/src/main/java/com/example/oscarsmoviesrequest/FillList.java
+++ oscars-movies-request/src/main/java/com/example/oscarsmoviesrequest/FillList.java
@@ -7,13 +7,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.oscarsmoviesrequest.models.GetResponse;
+import com.example.oscarsmoviesrequest.models.Post;
 import com.example.oscarsmoviesrequest.models.PostRequest;
+import com.example.oscarsmoviesrequest.models.PostResponse;
+import com.example.oscarsmoviesrequest.controller.WebController;
 
+@Service
 public class FillList {
 	private final RestTemplate restTemplate = new RestTemplate();
-
+	private final WebController webController = new WebController();
+	
 	public PostRequest[] createPostRequests(List<String[]> list) {
 		ListIterator<String[]> itr = list.listIterator();
 		
@@ -46,7 +53,7 @@ public class FillList {
 		return requests;	
 	}
 	
-	public Post createPost() {
+	public PostRequest createPost(List<String[]> list) {
 		String url = "https://jsonplaceholder.typicode.com/posts";
 		
 		HttpHeaders headers = new HttpHeaders();
@@ -55,23 +62,73 @@ public class FillList {
 	    // set `accept` header
 	    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
+	    Map<String, Object> entriesMap = new HashMap<String, Object>();
+	    for (String[] entries : list) {
+	    	//for (Object categories : entries) {
+	    		
+	    	//}
+	    	entriesMap.put("year", entries[0]);
+	    	entriesMap.put("category", entries[1]);
+	    	entriesMap.put("winner", entries[2]);
+	    	entriesMap.put("entity", entries[3]);
+	    }
 	    Map<String, Object> map = new HashMap<>();
-	    map.put("userId", 1);
+	    map.put("userId", 0);
 	    map.put("title", "Introduction to Spring Boot");
 	    map.put("body", "Spring Boot makes it easy to create stand-alone, production-grade Spring based Applications.");
 
 	    // build the request
-	    HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
-
+	    //HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+	    HttpEntity<Map<String, Object>> entity = new HttpEntity<>(entriesMap, headers);
+	    
 	    // send POST request
-	    ResponseEntity<Post> response = this.restTemplate.postForEntity(url, entity, Post.class);
-
+	    //ResponseEntity<Post> response = this.restTemplate.postForEntity(url, entity, Post.class);
+	    ResponseEntity<PostRequest> response = this.restTemplate.postForEntity(url, entity, PostRequest.class);
+	    
 	    // check response status code
 	    if (response.getStatusCode() == HttpStatus.CREATED) {
 	        return response.getBody();
 	    } else {
 	        return null;
 	    }
+	}
+	
+	public PostRequest[] getPostsAsObject() {
+		String url = "https://jsonplaceholder.typicode.com/posts";
+		return this.restTemplate.getForObject(url, PostRequest[].class);	//
+	}
+	
+	public Post[] getPostsWithUrlParameters(int userId) {
+		String url = "https://jsonplaceholder.typicode.com/posts?userId=2";
+		return this.restTemplate.getForObject(url, Post[].class, userId);
+	}
+	
+	public void sendPostRequests(PostRequest[] requests) {
+		for (int k = 0; k < requests.length; k++) {
+			PostResponse tester = webController.Post(requests[k]);
+			//GetResponse get = webController.Get(name)
+			System.out.println(tester.getMessage());
+		}	
+	}
+	
+	public void getEntityInfo(PostRequest[] requests) {
+		
+		for (int k = 0; k < requests.length; k++) {
+			PostRequest request = requests[k];
+			System.out.print(request.getYear() + " ");
+			System.out.print(request.getCategory() + " ");
+			System.out.print(request.getWinner() + " ");
+			System.out.println(request.getEntity() + " ");
+			System.out.println("Got here");
+		}
+		
+		for (int k = 0; k < requests.length; k++) {
+			PostRequest request = requests[k];
+			GetResponse tester = webController.Get(request.getYear(),
+					request.getCategory(),request.getWinner(),request.getEntity());
+			//GetResponse get = webController.Get(name)
+			System.out.println(tester.getMessage());
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -101,9 +158,16 @@ public class FillList {
 			System.out.println(request.getEntity() + " ");
 		}
 		
-		fill.createPost();
+		//PostRequest test = fill.createPost(list);
+		//System.out.println(test);
+		//PostRequest[] post = fill.getPostsAsObject();
+		//System.out.println(post);
 		//must call PostResponse and then GetResponse
 		
+		//use the postrequest array that's returned to call PostCheck
+		fill.sendPostRequests(requests);
+		fill.getEntityInfo(requests);
+		System.out.println("Successful");
 	}
 	
 	
--- oscars-movies-request/src/main/java/com/example/oscarsmoviesrequest/controller/WebController.java
+++ oscars-movies-request/src/main/java/com/example/oscarsmoviesrequest/controller/WebController.java
@@ -2,17 +2,26 @@ package com.example.oscarsmoviesrequest.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.oscarsmoviesrequest.models.PostRequest;
 import com.example.oscarsmoviesrequest.models.PostResponse;
+import com.example.oscarsmoviesrequest.FillList;
 import com.example.oscarsmoviesrequest.models.GetResponse;
 
 @RestController
 public class WebController {
 
-	@RequestMapping("/get")
+	@Autowired
+	private FillList fillList;
+	
+	//change Get method parameters to the category filtered
+	//original copy of Get method
+	/*@RequestMapping("/get")
 	public GetResponse Get(@RequestParam(value = "name",
 	defaultValue = "Robot") String name) {
 		GetResponse response = new GetResponse();
@@ -22,20 +31,81 @@ public class WebController {
 				+ ", " + response.getWinner() + ", " + response.getEntity() + "]");
 		return response;
 
+	}*/
+	
+	//Test copy of Get method
+	@GetMapping("/user")
+	public GetResponse Get(@RequestParam(value = "year") long year, String category, boolean winner, String entity) {
+		GetResponse response = new GetResponse();
+		//response.setId(1);
+		//response.setMessage("Your name is "+name);
+		System.out.print(year + " ");
+		System.out.print(category + " ");
+		System.out.print(winner + " ");
+		System.out.println(entity + " ");
+		System.out.println("Got here");
+		
+		response.setCategory(category);
+		response.setEntity(entity);
+		response.setWinner(winner);
+		response.setYear(year);
+		response.setMessage("Got [" + response.getYear() + ", " + response.getCategory()
+				+ ", " + response.getWinner() + ", " + response.getEntity() + "]");
+		return response;
+
 	}
 	
-	@RequestMapping(value = "/post", method = RequestMethod.POST)
-	public PostResponse Post(@RequestBody PostRequest inputPayload) {
+	@RequestMapping("/user/category")
+	public GetResponse GetByCategory(@PathVariable("category") String category) {
+		GetResponse response = new GetResponse();
+		//response.setId(1);
+		//response.setMessage("Your name is "+name);
+		//System.out.print(year + " ");
+		System.out.print(category + " ");
+		//System.out.print(winner + " ");
+		//System.out.println(entity + " ");
+		System.out.println("Got here");
+		
+		response.setCategory(category);
+		//response.setEntity(entity);
+		//response.setWinner(winner);
+		//response.setYear(year);
+		response.setMessage("Got: " + response.getCategory());
+		//response.setMessage("Got [" + response.getYear() + ", " + response.getCategory()
+		//		+ ", " + response.getWinner() + ", " + response.getEntity() + "]");
+		return response;
+
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public PostResponse Post(@RequestBody PostRequest inputPayload) {			//change this method to using array
 		PostResponse response = new PostResponse();
 		//response.setId(inputPayload.getId());
 		//response.setMessage("Hello " + inputPayload.getName());
 		//response.setExtra("Some text");
-		response.setMessage("Implemented [" + response.getYear() + ", " + response.getCategory()
-		+ ", " + response.getWinner() + ", " + response.getEntity() + "]");
 		response.setCategory(inputPayload.getCategory());
 		response.setEntity(inputPayload.getEntity());
 		response.setWinner(inputPayload.getWinner());
 		response.setYear(inputPayload.getYear());
+		response.setMessage("Implemented [" + response.getYear() + ", " + response.getCategory()
+		+ ", " + response.getWinner() + ", " + response.getEntity() + "]");
+		
 		return response;
 	}
+	
+	/*@RequestMapping(value = "/post", method = RequestMethod.POST)
+	public PostResponse PostCheck(@RequestBody PostRequest inputPayload) {			//change this method to using array
+		PostResponse response = new PostResponse();
+		//response.setId(inputPayload.getId());
+		//response.setMessage("Hello " + inputPayload.getName());
+		//response.setExtra("Some text");
+		response.setCategory(inputPayload.getCategory());
+		response.setEntity(inputPayload.getEntity());
+		response.setWinner(inputPayload.getWinner());
+		response.setYear(inputPayload.getYear());
+		response.setMessage("Implemented [" + response.getYear() + ", " + response.getCategory()
+		+ ", " + response.getWinner() + ", " + response.getEntity() + "]");
+		
+		return response;
+	}*/
 }
