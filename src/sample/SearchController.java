3cae572a94810c5e57cca2fc84905503335dfc39
Nicole Monroy <mmonroy2@student.sierracollege.edu>
2020-11-23 20:19:50 -0800

    Added method to display output when searching for movies by category
    Also added javafx sdk to lib

--- src/sample/SearchController.java
+++ src/sample/SearchController.java
@@ -2,13 +2,15 @@ package sample;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import com.opencsv.exceptions.CsvValidationException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -21,50 +23,101 @@ public class SearchController {
     private Button searchButton;
     @FXML
     private TextArea movieTextArea;
+    @FXML
+    private ListView movieList;
+    @FXML
+    private Tab tab1;
+
+    oscarSearch os = new oscarSearch();
+
+
+    int count = 0;  //counter to keep track of amount of times search was done
+
 
     public SearchController() {
     }
 
-    public void handleEnterKey(KeyEvent e) {
+    public void handleEnterKey(KeyEvent e) throws IOException, CsvValidationException {
         if (e.getCode() == KeyCode.ENTER) {
-            String input = this.searchField.getText();
-            this.displaySearchLabel.setText(input);
-            this.displaySearchLabel.setVisible(true);
+            if (count == 0) {
+                String input = searchField.getText();
+                displaySearchLabel.setText(input);
+                displaySearchLabel.setVisible(true);
+                movieTextArea.insertText(0, "Showing results for: " + input +"\n");
+                tab1.setText(" " + input + " ");
+                display();
+                count += 1;
+            }
+            else if (count >= 1)
+            {
+                movieTextArea.setText(" ");     //clear the search output
+                String input = searchField.getText();
+                displaySearchLabel.setText(input);
+                displaySearchLabel.setVisible(true);
+                movieTextArea.replaceText(0, 1, "Showing results for: " + input +"\n");
+                tab1.setText(input);
+                display();
+
+            }
         }
+        //display();
 
     }
 
     public void handleSearchButton(ActionEvent e) {
         try {
-            String input = this.searchField.getText();
-            this.displaySearchLabel.setText(input);
-            this.displaySearchLabel.setVisible(true);
-            this.movieTextArea.appendText(input);
+            if (count == 0) {
+                String input = searchField.getText();
+                displaySearchLabel.setText(input);
+                displaySearchLabel.setVisible(true);
+                movieTextArea.insertText(0, "Showing results for: " + input +"\n");
+                display();
+                count += 1;
+            }
+            else if ( count >= 1)
+            {
+                movieTextArea.setText(" ");
+                String input = searchField.getText();
+                displaySearchLabel.setText(input);
+                displaySearchLabel.setVisible(true);
+                movieTextArea.replaceText(0, 1, "Showing results for: " + input +"\n");
+                display();
+            }
+
         } catch (NullPointerException var3) {
-            System.out.println("please enter a movie or actor name");
+            var3.printStackTrace();
         } catch (Exception var4) {
             var4.printStackTrace();
         }
 
     }
 
-    public void display() {
-        String path = "C:\\Users\\Admin\\Desktop\\movie app\\src\\sample\\datahubio_oscar_data_csv.csv";
+    public void display() throws IOException, CsvValidationException {
+        oscarSearch os = new oscarSearch();
+        ArrayList<String[]> list = os.search(searchField.getText());
+        //int i = 10;
+        for (String[] arr : list )  //iterate over list of arrays
+        {
+            movieTextArea.appendText(arr[5] + ", (" + arr[0] + ") \n");
 
-        try {
-            Scanner s = (new Scanner(new File(path))).useDelimiter(",");
+            /*for (String s : arr )   //then iterate over each string in array
+            {
+                if (s.equalsIgnoreCase(searchField.getText()))    //if what the user searched for is found
+                {
+                    //System.out.println(s);
+                    for ( int i = 0; i < arr.length; i++)
+                    {
+                        movieTextArea.insertText(1, arr[i]);
+                        //movieTextArea.insertText(i+1, "\n");
+                        if (arr[i].equalsIgnoreCase("true") || arr[i].equalsIgnoreCase("false") )
+                        {
+                            movieTextArea.insertText(i, "\n");
+                        }
+                    }
+                }*/
 
-            while(s.hasNext()) {
-                if (s.hasNextInt()) {
-                    this.movieTextArea.appendText(s.nextInt() + " ");
-                    System.out.println("int" + s.nextInt());
-                } else {
-                    this.movieTextArea.appendText(s.next() + " ");
-                }
-            }
-        } catch (FileNotFoundException var3) {
-            System.err.println(var3);
         }
+
 
     }
 }
