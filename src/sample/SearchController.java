e35a8877d33415e9200860bf367cdc090ab77527
Nicole Monroy <mmonroy2@student.sierracollege.edu>
2020-11-29 20:23:22 -0800

    added executable jar file of projects
    also added ability within app to filter by winners only

--- src/sample/SearchController.java
+++ src/sample/SearchController.java
@@ -27,8 +27,10 @@ public class SearchController {
     private ListView movieList;
     @FXML
     private Tab tab1;
+    @FXML
+    private CheckBox filterCheck;
 
-    oscarSearch os = new oscarSearch();
+    //oscarSearch os = new oscarSearch();
 
 
     int count = 0;  //counter to keep track of amount of times search was done
@@ -37,51 +39,51 @@ public class SearchController {
     public SearchController() {
     }
 
+    public void zeroCount() throws IOException, CsvValidationException {
+        String input = searchField.getText();
+        displaySearchLabel.setText(input);
+        displaySearchLabel.setVisible(true);
+        movieTextArea.insertText(0, "Showing results for: " + input +"\n");
+        tab1.setText(" " + input + " ");
+        display();
+        count += 1;
+
+    }
+
+    public void nCount() throws IOException, CsvValidationException {
+        movieTextArea.setText(" ");     //clear the search output
+        String input = searchField.getText();
+        displaySearchLabel.setText(input);
+        displaySearchLabel.setVisible(true);
+        movieTextArea.replaceText(0, 1, "Showing results for: " + input +"\n");
+        tab1.setText(input);
+        display();
+
+    }
+
     public void handleEnterKey(KeyEvent e) throws IOException, CsvValidationException {
         if (e.getCode() == KeyCode.ENTER) {
             if (count == 0) {
-                String input = searchField.getText();
-                displaySearchLabel.setText(input);
-                displaySearchLabel.setVisible(true);
-                movieTextArea.insertText(0, "Showing results for: " + input +"\n");
-                tab1.setText(" " + input + " ");
-                display();
-                count += 1;
+                zeroCount();
             }
             else if (count >= 1)
             {
-                movieTextArea.setText(" ");     //clear the search output
-                String input = searchField.getText();
-                displaySearchLabel.setText(input);
-                displaySearchLabel.setVisible(true);
-                movieTextArea.replaceText(0, 1, "Showing results for: " + input +"\n");
-                tab1.setText(input);
-                display();
-
+                nCount();
             }
         }
-        //display();
 
     }
+
+
 
     public void handleSearchButton(ActionEvent e) {
         try {
             if (count == 0) {
-                String input = searchField.getText();
-                displaySearchLabel.setText(input);
-                displaySearchLabel.setVisible(true);
-                movieTextArea.insertText(0, "Showing results for: " + input +"\n");
-                display();
-                count += 1;
+                zeroCount();
             }
             else if ( count >= 1)
             {
-                movieTextArea.setText(" ");
-                String input = searchField.getText();
-                displaySearchLabel.setText(input);
-                displaySearchLabel.setVisible(true);
-                movieTextArea.replaceText(0, 1, "Showing results for: " + input +"\n");
-                display();
+                nCount();
             }
 
         } catch (NullPointerException var3) {
@@ -94,27 +96,24 @@ public class SearchController {
 
     public void display() throws IOException, CsvValidationException {
         oscarSearch os = new oscarSearch();
-        ArrayList<String[]> list = os.search(searchField.getText());
+        ArrayList<String[]> list = os.searchCategory(searchField.getText());
         //int i = 10;
         for (String[] arr : list )  //iterate over list of arrays
         {
-            movieTextArea.appendText(arr[5] + ", (" + arr[0] + ") \n");
-
-            /*for (String s : arr )   //then iterate over each string in array
+            if (filterCheck.isSelected())
             {
-                if (s.equalsIgnoreCase(searchField.getText()))    //if what the user searched for is found
+                for (String s : arr)
                 {
-                    //System.out.println(s);
-                    for ( int i = 0; i < arr.length; i++)
-                    {
-                        movieTextArea.insertText(1, arr[i]);
-                        //movieTextArea.insertText(i+1, "\n");
-                        if (arr[i].equalsIgnoreCase("true") || arr[i].equalsIgnoreCase("false") )
-                        {
-                            movieTextArea.insertText(i, "\n");
-                        }
+                    if (s.equalsIgnoreCase("true")) {
+                        movieTextArea.appendText(arr[5] + ", (" + arr[0] + ") \u2606 \n");
                     }
-                }*/
+                }
+
+            }
+            else {
+                movieTextArea.appendText(arr[5] + ", (" + arr[0] + ") \n");
+            }
+
 
         }
 
---
+++ src/sample/oscarSearch.java
@@ -0,0 +1,103 @@
+package sample;
+
+import java.awt.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class oscarSearch {
+
+    public oscarSearch() throws FileNotFoundException {
+    }
+
+    public static void main(String[] args) throws CsvValidationException, IOException, FileNotFoundException {
+        oscarSearch os = new oscarSearch();
+        //ArrayList<String[]> test = os.searchCategory();
+        System.out.println("test");
+
+
+        /*GET SEARCH PARAM FROM USER*/
+    /*Scanner input = new Scanner(System.in);
+
+    /*FIND WHICH COLUMN WE WILL SEARCH*/
+        /*LOOPS UNTIL EITHER 1, 2, OR 3 ARE ENTERED*/
+   /*do {
+      System.out.println("Would you like to search :");
+      System.out.println("                          (1)By year?");
+      System.out.println("                          (2)By category?");
+      System.out.println("                          (3)By title?");
+      int tempParam = input.nextInt();
+      if(tempParam == 1) {
+        searchParam = 0;
+      }else if(tempParam == 2) {
+        searchParam = 3;
+      }else if(tempParam ==3) {
+        searchParam = 5;
+      }else {
+        searchParam = -1;
+      }
+    }while(searchParam < 0);
+
+    /*GET STRING TO SEARCH FOR*/
+        //input.nextLine();
+        //System.out.println("Enter search : ");
+        //String searchWord = input.nextLine();
+
+        /*ONLY SEE WINNERS?*/
+        /*LOOPS UNTIL INPUT IS CORRECT*/
+    /*do {
+      System.out.println("Would you like to only see films that won? (y) or (n)");
+      filter = input.next().charAt(0);
+      if(filter == 'y' || filter == 'Y') {
+        filterWin = true;
+      }
+    }while(filter != 'y' && filter != 'n' && filter != 'Y' && filter != 'N');
+    input.close();*/
+
+
+    }
+
+    public ArrayList<String[]> searchCategory(String searchWord) throws IOException, CsvValidationException {
+        /*DECLARE STRUCTURES*/
+        CSVReader reader = new CSVReader(new FileReader("C:\\Users\\Admin\\Desktop\\test\\src\\sample//oscarData.csv"));
+        //CSVReader reader = new CSVReader(new FileReader("oscarData.csv"));//object for CSV operations
+        String[] nextLine;                                                  //temp storage for line being read
+        ArrayList<String[]> result = new ArrayList<String[]>();;           //ArrayList to hold results
+        int searchParam;                                                    //indicates whether we search for year, category or title
+        boolean filterWin = false;                                          //controls filter by winners in search
+        char filter;                                                        //user input for filter by winner
+
+        /*THE SEARCH*/
+        while((nextLine = reader.peek()) != null) {                         //peeks next line without advancing iterator
+            if(filterWin) {
+                if(nextLine[3].equalsIgnoreCase(searchWord) && Boolean.valueOf(nextLine[6])) {        //if category matches search
+                    result.add(reader.readNext());                                //populate result List
+                } else {
+                    reader.skip(1);
+                }
+            }else {
+                if(nextLine[3].equalsIgnoreCase(searchWord)) {          //if category matches search
+                    result.add(reader.readNext());                                  //populate result List
+                } else {
+                    reader.skip(1);
+                }
+            }
+        }
+
+        System.out.println(result.size() + " matches found.");
+
+        for(String temp[]: result) {
+
+            System.out.println(Arrays.toString(temp));
+        }
+        return result;
+
+    }
+}
